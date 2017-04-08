45a4eefe4f170330a6782c6b56625dde04476765
soleribbon <ivar202@gmail.com>
2017-04-08 14:20:53 -0700

    Commit message

--- proxi.py
+++ proxi.py
@@ -5,16 +5,23 @@ print "\n"
 print "MAKE SURE YOU HAVE 'proxies.txt' FILE IN SAME DIRECTORY!"
 print "\n"
 
-port = (raw_input("Port:"))
-user = (raw_input("User: "))
-passw = (raw_input("Pass: "))
+userpass = raw_input("Do the proxies need :user:pass? (y/n): ")
+
+
 
 new_lines = []
-
-with open("proxies.txt") as f:
-       new_lines = [line.strip("\n") + ":" + (port) + ":" + (user) + ":" + (passw) for line in f]
-with open("proxies.txt","w") as f:
-       f.write("\n".join(new_lines))
-
-
+if userpass == "y":
+    port = (raw_input("Port:"))
+    user = (raw_input("User: "))
+    passw = (raw_input("Pass: "))
+    with open("proxies.txt") as f:
+        new_lines = [line.strip("\n") + ":" + (port) + ":" + (user) + ":" + (passw) for line in f]
+    with open("proxies.txt","w") as f:
+        f.write("\n".join(new_lines))
+if userpass == "n":
+    port = (raw_input("Port:"))
+    with open("proxies.txt") as f:
+        new_lines = [line.strip("\n") + ":" + (port) for line in f]
+    with open("proxies.txt","w") as f:
+        f.write("\n".join(new_lines))
 

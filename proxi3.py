91ae18c5b869a275c766ed285dc30567f83f2bd9
soleribbon <ivar202@gmail.com>
2017-04-08 20:49:15 -0700

    Create proxi3.py

---
+++ proxi3.py
@@ -0,0 +1,36 @@
+import os
+
+print ("Tool For Proxy Makers\nBy @Masterkidravi")
+print ("\n")
+print ("MAKE SURE YOU HAVE 'proxies.txt' FILE IN SAME DIRECTORY!")
+print ("\n")
+
+userpass = input("Do the proxies need :user:pass? (y/n): ")
+
+port = input("Port: ")
+
+
+new_lines = []
+if userpass == "y":
+
+    user = input("User: ")
+    passw = input("Pass: ")
+    with open("proxies.txt") as f:
+        new_lines = [line.strip("\n") + ":" + (port) + ":" + (user) + ":" + (passw) for line in f]
+        
+    with open("proxies.txt","w") as f:
+        f.write("\n".join(new_lines))
+        f.close() 
+    print ("DONE!")
+
+
+    
+        
+if userpass == "n":
+
+    with open("proxies.txt") as f:
+        new_lines = [line.strip("\n") + ":" + (port) for line in f]
+    with open("proxies.txt","w") as f:
+        f.write("\n".join(new_lines))
+        f.close()
+    print ("Done!")
